df259b97f6b78f7425254c422a5fe361a9183b8f
MostafaSalahAbdallah <mostafasalahabdallah1@gmail.com>
2026-07-30 12:29:18 +0300

    List page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from '../src/pages/Home'
+import List from "./pages/List";
 function App() {
 
   return (
